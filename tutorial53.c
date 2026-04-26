a6f4827913340d73649835c1e00a3651665c9feb
Nishant <nishantofficial74@gmail.com>
2026-04-26 14:39:43 +0530

    second commit

---
+++ tutorial53.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a = 34;
+    int *ptr = &a;
+    ptr = NULL;
+
+    // printf("The Address of a is %d\n", ptr);
+    // printf("The VAlue of a is %d\n", *ptr);
+
+    if (ptr != NULL)
+    {
+        printf("The Value of Pointer is %d\n", *ptr);
+    }
+    else
+    {
+        printf("The Pointer is a Null Pointer and cannot be defreferenced ");
+    }
+
+    return 0;
+}
